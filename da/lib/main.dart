c47b0c1bbe210148ceed15cb36914b4c3472b537
LuZo <lukas.zorn@protonmail.com>
2023-11-15 19:20:36 +0100

    Now it should Run fine, there still a few bugs (Style,Permission request) and the rest will be added

--- da/lib/main.dart
+++ da/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'landing_page.dart';
+import 'app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,83 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Schnell test obs rennt',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
-      ),
+      title: 'Diploma App',
+      theme: appTheme(),
       home: const LandingPage(),
-    );
-  }
-}
-
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Landing Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const WebViewScreen(url: "http://192.168.1.85:80")),
-                );
-              },
-              child: const Text('Zu den Algos'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const WebViewScreen(url: "https://www.bulme.at/")),
-                );
-              },
-              child: const Text('Homepage'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WebViewScreen(
-                          url:
-                              "https://docs.google.com/document/d/1lz6akasL-0GFXRB1J_aWF2DIi-dOwn3Wv29YsnuJc-Q/edit?usp=sharing")),
-                );
-              },
-              child: const Text('Zur Diplomarbeit'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class WebViewScreen extends StatelessWidget {
-  final String url;
-  const WebViewScreen({Key? key, required this.url}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Webview'),
-      ),
-      body: WebView(
-        initialUrl: url,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
     );
   }
 }
