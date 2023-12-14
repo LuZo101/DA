de362b0f957deb1b523988cfba2357134c5b3a70
Lukas Zorn <lukas.zorn@protonmail.com>
2023-12-14 23:28:07 +0100

    Tryin to fix the problem with the database
    	modified:   Robin-Star-Algorythm/insertData.php

--- Robin-Star-Algorythm/insertData.php
+++ Robin-Star-Algorythm/insertData.php
@@ -11,14 +11,12 @@ $dbname = "diplomarbeit";
 try {
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Überprüfen, ob die Verbindung hergestellt wurde
     if ($conn->connect_error) {
         throw new Exception("Connection failed: " . $conn->connect_error);
     }
 
     $data = json_decode(file_get_contents('php://input'), true);
 
-    // Validieren der Eingabedaten
     if (!isset($data['finalPathCounter'], $data['visitedCellCounter'], $data['timeTaken'], $data['algorithmId'])) {
         throw new Exception("Invalid input data");
     }
@@ -28,7 +26,6 @@ try {
     $timeTaken = $conn->real_escape_string($data['timeTaken']);
     $algorithmId = $conn->real_escape_string($data['algorithmId']);
 
-    // Prepared Statement verwenden
     $stmt = $conn->prepare("INSERT INTO data (algorithm_id, path_length, cells_visited, elapsed_time) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("iiis", $algorithmId, $finalPathCounter, $visitedCellCounter, $timeTaken);
 
@@ -36,13 +33,10 @@ try {
         throw new Exception("Error inserting data: " . $stmt->error);
     }
 
-    echo "Data inserted successfully";
     echo json_encode(["message" => "Data inserted successfully"]);
 } catch (Exception $e) {
-    // Fehlerprotokollierung
     error_log($e->getMessage());
-    // Anzeige einer allgemeinen Fehlermeldung
-    echo "An error occurred. Please try again later.";
+    echo json_encode(["error" => "An error occurred. Please try again later."]);
 } finally {
     if (isset($conn)) {
         $conn->close();
